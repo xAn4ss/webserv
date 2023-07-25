7fa79176f065213b0183bac21a350306ea3b2ed5
xAn4ss <13anas.oukharta37@gmail.com>
2023-07-25 04:04:19 +0100

    parsing conf file

--- Config.hpp
+++ Config.hpp
@@ -4,7 +4,10 @@
 #include <fstream>
 #include <iostream>
 #include <stack>
-
+#include <fcntl.h>
+#include <unistd.h> 
+#include <vector>
+#include <iterator>
 class Config
 {
 private:
@@ -19,34 +22,57 @@ public:
 
 int Config::check_brackets(){
     std::stack<char> br;
-    std::ifstream f(this->config_file);
-    if (!f.is_open())
-        return -1;
-    std::string buf ;
-    while (std::getline(f, buf)){
-        if (buf.find("{", 0) != std::string::npos)
+    int i = 0;
+    while ((i = config.find_first_of("{}", i)) != -1)
+    {
+        if (config.at(i) == '{'){
             br.push('{');
-        else if (buf.find("}", 0) != std::string::npos)
+        }else
             br.pop();
-        std::cout <<"." << buf << std::endl;
+        i++;
     }
     if (!br.empty())
         return -1;
-    
     return 0;
 }
 
 int Config::parse_config()
 {
+    char s[1042];
+    int fd;
+
+    std::string buf ;
+    if ((fd = open(this->config_file.c_str(), O_RDONLY)) < 0)
+    {
+        std::cout << "Can't open." << std::endl;
+        return -1;
+    }
+    int rd = read(fd, s, 1042);
+    if (rd > 0){
+        s[rd] = '\0';
+    }else{
+        std::cout << "Can't read." << std::endl;
+        return -1;
+    }
+    config = s;
     if (check_brackets() < 0)
         return -1;
-    std::ifstream file(this->config_file);
-    if (!file.is_open())
-        return -1;
-    std::string buf ;
-    while (std::getline(file, buf)){
-        if 
+    int i = 0;
+    std::vector<std::string> configVec;
+    char *x = strtok(s, " \t\n");
+    configVec.push_back(x);
+    while (x){
+        // std::cout <<"->"<< config[i] <<""<< std::endl;
+        // std::cout <<".."<< i << std::endl;
+        std::cout <<"->"<< x << std::endl;
+        configVec.push_back(x);
+        x = strtok(NULL, " \t\n");
     }
+    std::vector<std::string>::iterator it = configVec.begin();
+    std::cout << "-> " << config << "." << std::endl;
+    // " \t\n"
+    
+    return 0;
 }
 
 Config::Config(std::string file):config_file(file)
